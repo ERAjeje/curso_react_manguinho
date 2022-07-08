491ccc6a9793f9d72161ded9ac9c29ad310cde1f
Edson Ajeje <edson.ajeje@terra.com.br>
2022-07-08 18:47:16 -0300

    test: implemented RemoteAuth with body params

--- src/data/usecases/authentication/remote-auth.test.ts
+++ src/data/usecases/authentication/remote-auth.test.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import RemoteAuthentication from './remote-auth'
-
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
@@ -20,7 +20,12 @@ describe('RemoteAuthentication', () => {
   it('Should call HttpPostClient with correct URL', async () => {
     const url = 'http://localhost:3000/auth'
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+  it('Should call HttpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    await sut.auth(mockAuthentication())
+    expect(httpPostClientSpy.body).toEqual(mockAuthentication())
   })
 })
---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AuthenticationParams } from '../usecases/authentication'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: 'email@email.com',
+  password: 'at123@2ac'
+})
